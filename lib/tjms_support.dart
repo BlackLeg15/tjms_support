fa6f2434b867b4cd701a6c48166c1e267fa08cfb
Adby Santos <adby.santos15@outlook.com>
2023-01-09 15:00:23 -0300

    Initial commit

---
+++ lib/tjms_support.dart
@@ -0,0 +1,6 @@
+library tjms_support;
+
+export 'controllers/support_controller.dart';
+export 'models/support_model.dart';
+export 'repositories/support_repository.dart';
+export 'views/support_view.dart';
---
+++ lib/views/support_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SupportView extends StatefulWidget {
+  const SupportView({super.key});
+
+  @override
+  State<SupportView> createState() => _SupportViewState();
+}
+
+class _SupportViewState extends State<SupportView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
